97ac4c2440fc4c1245e5ae9d55e6e2f666672c12
tommy-tran <tome7@live.com>
2017-06-17 09:31:33 -0400

    Border, Icons, auto-create grid system

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
+	public Transform m_emptySprite;
+	public int m_height = 30;
+	public int m_width = 10;
+	public int m_header = 8;
+
 	// Use this for initialization
+
+	Transform[,] m_grid;
+
+	void Awake()
+	{
+		m_grid = new Transform[m_width, m_height];
+	}
+
 	void Start () {
-		
+		DrawEmptyCells ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void DrawEmptyCells() 
+	{
+		if (m_emptySprite != null) {
+			for (int y = 0; y < m_height - m_header; y++) {
+				for (int x = 0; x < m_width; x++) {
+					Transform clone;
+					clone = Instantiate (m_emptySprite, new Vector3 (x, y, 0), Quaternion.identity) as Transform;
+					clone.name = "Board Space ( x = " + x.ToString () + " , y =" + y.ToString () + " )";
+					clone.transform.parent = transform;
+				}
+			}
+		} else 
+		{
+			Debug.Log ("WARNING! Assign emptySprite object!");
+		}
+
 	}
 }
