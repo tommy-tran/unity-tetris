e6614e587179645bf04c9d1e2d83d2ec79d7c4c5
tommy-tran <tome7@live.com>
2017-06-18 01:21:10 -0400

    Shape landing, rotate, drop and clear rows

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -37,6 +37,34 @@ public class Board : MonoBehaviour {
 
 	}
 
+	bool isWithinBoard(int x, int y) {
+		return (x >= 0 && x < m_width && y >= 0);
+	}
+
+	bool isOccupied(int x, int y, Shape shape)
+	{
+		return (m_grid [x, y] != null && m_grid [x, y].parent != shape.transform);
+	}
+
+
+	public bool isValidPosition(Shape shape)
+	{
+		foreach (Transform child in shape.transform) {
+			Vector2 pos = Vectorf.Round (child.position);
+
+			if (!isWithinBoard ((int)pos.x, (int)pos.y)) {
+				return false;
+			}
+
+			if (isOccupied ((int)pos.x, (int)pos.y, shape)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
 	// draw our empty board with our empty sprite object
 	void DrawEmptyCells() {
 		if (m_emptySprite)
@@ -58,5 +86,65 @@ public class Board : MonoBehaviour {
 		}
 	}
 
+	public void StoreShapeInGrid(Shape shape) {
+		if (shape == null) {
+			return;
+		}
+
+		foreach (Transform child in shape.transform) 
+		{
+			Vector2 pos = Vectorf.Round (child.position);
+			m_grid [(int)pos.x, (int)pos.y] = child;
+		}
+	}
+
+	bool IsComplete(int y) 
+	{
+		for (int x = 0; x < m_width; ++x) 
+		{
+			if (m_grid [x, y] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	void ClearRow(int y) 
+	{
+		for (int x = 0; x < m_width; ++x) {
+			if (m_grid [x, y] != null) {
+				Destroy (m_grid [x, y].gameObject);
+			}
+			m_grid [x, y] = null;
+		}
+	}
+
+	void ShiftOneRowDown(int y) {
+		for (int x = 0; x < m_width; ++x) {
+			if (m_grid [x, y] != null) {
+				m_grid [x, y - 1] = m_grid [x, y];
+				m_grid [x, y] = null;
+				m_grid [x, y - 1].position += new Vector3 (0, -1, 0);
+			}
+		}
+	}
+
+	void ShiftRowsDown(int startY) {
+		for (int i = startY; i < m_height; ++i) {
+			ShiftOneRowDown (i);
+		}
+	}
+
+	public void ClearAllRows()
+	{
+		for (int y = 0; y < m_height; ++y) {
+			if (IsComplete (y)) {
+				ClearRow (y);
+				ShiftRowsDown (y + 1);
+				y--;
+			}
+		}
+
+	}
 
 }
--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -9,6 +9,28 @@ public class GameController : MonoBehaviour {
 	// reference to our spawner 
 	Spawner m_spawner;
 
+	// active
+	Shape m_activeShape;
+
+	public float m_dropInterval = 0.7f;
+	float m_timeToDrop;
+	float m_timeToNextKey;
+
+
+	[Range(0.02f, 1f)]
+	public float m_keyRepeatRateLeftRight = 0.15f;
+	float m_timeToNextKeyLeftRight;
+
+
+	[Range(0.01f, 1f)]
+	public float m_keyRepeatRateDown = 0.01f;
+	float m_timeToNextKeyDown;
+
+	[Range(0.02f, 1f)]
+	public float m_keyRepeatRateRotate = 0.02f;
+	float m_timeToNextKeyRotate;
+
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -21,33 +43,88 @@ public class GameController : MonoBehaviour {
 		m_gameBoard = GameObject.FindObjectOfType<Board>();
 		m_spawner = GameObject.FindObjectOfType<Spawner>();
 
-
-		if (m_spawner)
-		{
-			m_spawner.transform.position = Vectorf.Round(m_spawner.transform.position);
-		}
+		m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+		m_timeToNextKeyDown = Time.time + m_keyRepeatRateDown;
+		m_timeToNextKeyRotate = Time.time + m_keyRepeatRateRotate;
 			
-
 		if (!m_gameBoard)
 		{
 			Debug.LogWarning("WARNING!  There is no game board defined!");
 		}
 
-		if (!m_spawner)
-		{
-			Debug.LogWarning("WARNING!  There is no spawner defined!");
+		if (!m_spawner) {
+			Debug.LogWarning ("WARNING!  There is no spawner defined!");
+		} else {
+			m_spawner.transform.position = Vectorf.Round(m_spawner.transform.position);
+			if (!m_activeShape) {
+				m_activeShape = m_spawner.SpawnShape ();
+			}
+		}
+	}
+
+	void LandShape ()
+	{
+
+		m_timeToNextKeyLeftRight = Time.time;
+		m_timeToNextKeyDown = Time.time;
+		m_timeToNextKeyRotate = Time.time;
+
+		m_activeShape.MoveUp ();
+		m_gameBoard.StoreShapeInGrid (m_activeShape);
+		m_activeShape = m_spawner.SpawnShape ();
+
+		m_gameBoard.ClearAllRows ();
+	}
+
+	void PlayerInput ()
+	{
+		if (Input.GetButton ("MoveRight") && Time.time > m_timeToNextKeyLeftRight) {
+			m_activeShape.MoveRight ();
+			m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+			if (!m_gameBoard.isValidPosition (m_activeShape)) {
+				m_activeShape.MoveLeft ();
+			}
+		}
+		else if (Input.GetButton ("MoveLeft") && Time.time > m_timeToNextKeyLeftRight) {
+			m_activeShape.MoveLeft ();
+			m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+			if (!m_gameBoard.isValidPosition (m_activeShape)) {
+				m_activeShape.MoveRight ();
+			}
+		}
+		else if (Input.GetButtonDown ("Rotate") && Time.time > m_timeToNextKeyRotate) {
+			m_activeShape.RotateRight ();
+			m_timeToNextKeyRotate = Time.time + m_keyRepeatRateRotate;
+			if (!m_gameBoard.isValidPosition (m_activeShape)) {
+				m_activeShape.RotateLeft ();
+			}
 		}
 
+		if (Input.GetButton("MoveDown") && (Time.time > m_timeToNextKeyDown) || (Time.time > m_timeToDrop)) {
+			
+			m_timeToDrop = Time.time + m_dropInterval;
+			m_timeToNextKeyDown = Time.time + m_keyRepeatRateDown;
+
+			m_activeShape.MoveDown ();
+			if (!m_gameBoard.isValidPosition (m_activeShape)) {
+				LandShape ();
+			}
+
+		}
+		// if we don't have a spawner or gameBoard just don't run the game
+		if (!m_gameBoard || !m_spawner) {
+			return;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		// if we don't have a spawner or gameBoard just don't run the game
-		if (!m_gameBoard || !m_spawner)
-		{
+		if (!m_gameBoard || !m_spawner || !m_activeShape) {
 			return;
 		}
+
+		PlayerInput ();
 
 	}
 }
