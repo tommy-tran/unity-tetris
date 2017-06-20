40fbf31f5a2d0d562dc6c33a8af5d8de616c5ab0
tommy-tran <tome7@live.com>
2017-06-20 06:36:57 -0400

    Started creating Holder, made UI and started Holder script

---
+++ Assets/Scripts/Core/Holder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holder : MonoBehaviour {
+
+	public Transform m_holderTransform;
+	public Shape m_heldShape = null;
+	float m_scale = 0.5f;
+
+	public void Catch(Shape shape) 
+	{
+		if (m_heldShape) 
+		{
+			Debug.LogWarning ("HOLDER WARNING! Release a shape before you try to hold!");
+			return
+		}
+
+		if (!shape) 
+		{
+			Debug.LogWarning ("HOLDER WARNING! Invalid Shape!");
+			return
+		}
+
+		if (m_holderTransform) 
+		{
+
+
+		}
+	}
+}
